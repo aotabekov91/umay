4686c9b3181af159636fee9f0f7594fbd32b2971
adam <adam@myworld.net>
2023-07-29 21:28:21 +0200

    snips

---
+++ umay/parser/snips_parser__.py
@@ -0,0 +1,13 @@
+class SnipsParser:
+
+    def __init__(self, lan='en'):
+
+        self.lan=lan
+        self.files=[]
+        self.modes={}
+
+    def add(self, paths): pass
+
+    def fit(self): pass
+
+    def parse(self, text, mode=None, prob=.5): return text
